8e4cbe033b2190b25ab74df73eac9ecf3b608179
kmagai <koki.kumagai@dena.com>
2016-05-10 20:42:35 +0900

    better print

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,12 @@
 /*
-Fugo(富豪), meaning a rich man in Japanese, is a stock portfolio manager for japanese stock market.
+fugo, meaning a rich man in Japanese, is yet another stock portfolio manager.
 */
 package main
 
-import (
-	"fmt"
-
-	"github.com/kmagai/fugo"
-)
+import "github.com/kmagai/fugo"
 
 func main() {
 	portfolio := fugo.GetPortfolio()
 	portfolio = portfolio.Update()
-	for _, stock := range portfolio.Stocks {
-		// TODO: show formatted
-		fmt.Println(stock)
-	}
+	portfolio.PrintStocks()
 }
--- stock.go
+++ stock.go
@@ -3,9 +3,10 @@ package fugo
 import "time"
 
 type Stock struct {
-	Code         string    `json:"t"` // Depending on the market (f.g. code for TYO, ticker for NASDAQ...etc)
-	Name         string    `json:"name"`
-	Price        float64   `json:"l_fix,string"`
-	ClosingPrice int       `json:"pcls_fix,string"`
-	UpdatedAt    time.Time `json:"lt_dts,string"`
+	Code          string    `json:"t"` // Depending on the market (f.g. integer code for TYO, ticker for NASDAQ...etc)
+	Name          string    `json:"name"`
+	Price         float64   `json:"l_fix,string"`
+	Change        float64   `json:"c_fix,string"`
+	ChangePercent float64   `json:"cp_fix,string"`
+	UpdatedAt     time.Time `json:"lt_dts,string"`
 }
