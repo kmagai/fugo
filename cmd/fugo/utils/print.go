414b931376891728a500128d366b6f46ebf6d752
kmagai <koki.kumagai@dena.com>
2016-05-15 11:36:47 +0900

    tidy cluttered print logic

--- cmd/fugo/utils/print.go
+++ cmd/fugo/utils/print.go
@@ -1,44 +1,57 @@
 package utils
 
 import (
-	"fmt"
 	"math"
 
 	"github.com/fatih/color"
 	"github.com/kmagai/fugo"
 )
 
+var (
+	defaultColor     = color.New(color.FgBlack)
+	headColor        = color.New(color.FgGreen, color.Underline)
+	priceColor       = color.New(color.FgBlack, color.BgHiWhite, color.Bold)
+	priceChangeColor = color.New(color.FgBlack, color.Bold)
+)
+
 func PrintStocks(portfolio *fugo.Portfolio) {
 	if len(portfolio.Stocks) > 0 {
 		for _, stock := range portfolio.Stocks {
-			fmt.Println("------------------------------------")
-			headColor := color.New(color.FgGreen, color.Underline)
-			priceColor := color.New(color.FgBlack, color.BgHiWhite, color.Bold)
-			priceChangeColor := color.New(color.FgBlack, color.Bold)
-			headColor.Printf(stock.Name+" (%s)\n", stock.Code)
-			const layout = "2006-01-02 15:04:05"
-			fmt.Println(stock.UpdatedAt.Format(layout))
-			priceColor.Print(stock.Price)
-
-			if stock.Change > 0 {
-				priceChangeColor.Print(color.RedString("↑") + " ")
-				priceChangeColor.Print(roundAt(stock.Change, 2))
-				priceChangeColor.Print(" (")
-				priceChangeColor.Print(roundAt(stock.ChangePercent, 2))
-				priceChangeColor.Println("%)")
-			} else {
-				priceChangeColor.Print(color.BlueString("↓") + " ")
-				priceChangeColor.Print(roundAt(stock.Change, 2))
-				priceChangeColor.Print("(")
-				priceChangeColor.Print(roundAt(stock.ChangePercent, 2))
-				priceChangeColor.Println("%)")
-			}
+			printStock(stock)
+			defaultColor.Println("\n")
 		}
-		fmt.Println("------------------------------------")
 	} else {
-		fmt.Println("Nothing to print!")
-		fmt.Println("TODO: SHOW HELP MESSAGE HERE")
+		defaultColor.Println("Nothing to print!")
+		defaultColor.Println("TODO: SHOW HELP MESSAGE HERE")
 	}
+}
+
+func printStock(stock fugo.Stock) {
+	headColor.Printf(stock.Name+" (%s)\n", stock.Code)
+	const layout = "2006-01-02 15:04:05"
+	defaultColor.Println(stock.UpdatedAt.Format(layout))
+	priceColor.Print(stock.Price)
+	if stock.Change > 0 {
+		printPriceUp(stock)
+	} else {
+		printPriceDown(stock)
+	}
+}
+
+func printPriceUp(stock fugo.Stock) {
+	priceChangeColor.Print(color.RedString("↑") + " ")
+	priceChangeColor.Print(roundAt(stock.Change, 2))
+	priceChangeColor.Print(" (")
+	priceChangeColor.Print(roundAt(stock.ChangePercent, 2))
+	priceChangeColor.Println("%)")
+}
+
+func printPriceDown(stock fugo.Stock) {
+	priceChangeColor.Print(color.BlueString("↓") + " ")
+	priceChangeColor.Print(roundAt(stock.Change, 2))
+	priceChangeColor.Print("(")
+	priceChangeColor.Print(roundAt(stock.ChangePercent, 2))
+	priceChangeColor.Println("%)")
 }
 
 func roundAt(f float64, roundAt int) float64 {
