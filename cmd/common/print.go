8f64f53a2c0a00a8fd54c88e960579ff64cde2a2
kmagai <koki.kumagai@dena.com>
2016-05-27 14:24:25 +0900

    fix readablility problem

--- cmd/common/print.go
+++ cmd/common/print.go
@@ -3,58 +3,26 @@ package common
 import (
 	"math"
 
-	"github.com/fatih/color"
+	"fmt"
 	"github.com/kmagai/fugo"
-)
-
-var (
-	defaultColor     = color.New(color.FgBlack)
-	headColor        = color.New(color.FgGreen, color.Underline)
-	priceColor       = color.New(color.FgBlack, color.BgHiWhite, color.Bold)
-	priceChangeColor = color.New(color.FgBlack, color.Bold)
+	"github.com/olekukonko/tablewriter"
+	"os"
 )
 
 // ShowPortfolio prints stock data in portfolio
 func ShowPortfolio(portfolio *fugo.Portfolio) {
-	if len(portfolio.Stocks) > 0 {
-		for _, stock := range portfolio.Stocks {
-			printStock(stock)
-			defaultColor.Println()
-			defaultColor.Println()
-		}
-	} else {
-		defaultColor.Println("No stock data found")
-		defaultColor.Println("You may need to add stock to your portfolio")
-		defaultColor.Println("add it like `fugo add [CODE]`")
-	}
-}
+	fmt.Println()
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"CODE", "NAME", "PRICE", "CHANGE", "LAST_UPDATE"})
+	table.SetBorder(false)
+	table.SetAlignment(tablewriter.ALIGN_RIGHT)
 
-func printStock(stock fugo.Stock) {
-	headColor.Printf(stock.Name+" (%s)\n", stock.Code)
 	const layout = "2006-01-02 15:04:05"
-	defaultColor.Println(stock.UpdatedAt.Format(layout))
-	priceColor.Print(stock.Price)
-	if stock.Change > 0 {
-		printPriceUp(stock)
-	} else {
-		printPriceDown(stock)
+	for _, stock := range portfolio.Stocks {
+		table.Append([]string{stock.Code, stock.Name, fmt.Sprint(stock.Price), fmt.Sprint(roundAt(stock.Change, 2)) + " (" + fmt.Sprint(roundAt(stock.ChangePercent, 2)) + "%)", stock.UpdatedAt.Format(layout)})
 	}
-}
-
-func printPriceUp(stock fugo.Stock) {
-	priceChangeColor.Print(color.RedString("↑") + " ")
-	priceChangeColor.Print(roundAt(stock.Change, 2))
-	priceChangeColor.Print(" (")
-	priceChangeColor.Print(roundAt(stock.ChangePercent, 2))
-	priceChangeColor.Println("%)")
-}
-
-func printPriceDown(stock fugo.Stock) {
-	priceChangeColor.Print(color.BlueString("↓") + " ")
-	priceChangeColor.Print(roundAt(stock.Change, 2))
-	priceChangeColor.Print("(")
-	priceChangeColor.Print(roundAt(stock.ChangePercent, 2))
-	priceChangeColor.Println("%)")
+	table.Render()
+	fmt.Println()
 }
 
 func roundAt(f float64, roundAt int) float64 {
